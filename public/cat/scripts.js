76e5f2396e78ad4e412e0ae4302fe3e5d9fb58f0
Antonio Placido <aplacido1992@gmail.com>
2020-07-02 18:08:03 -0400

    Current Q1

--- public/cat/scripts.js
+++ public/cat/scripts.js
@@ -1,1 +1,11 @@
 // here there be JS, yarrr ☠️
+var userinput = ``;
+
+const messageInput = document.querySelector("#userinput");
+
+const sendMessage = (event) => {
+  // prevent the default "page reload" from occurring.
+  event.preventDefault();
+
+  console.log(messageInput.value.);
+};
