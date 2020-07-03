cf4ed23a135241648baf08702300617672a0e6d7
Antonio Placido <aplacido1992@gmail.com>
2020-07-03 11:34:45 -0400

    Question 3

--- public/parrot/scripts.js
+++ public/parrot/scripts.js
@@ -1,5 +1,5 @@
-const messageInput = document.querySelector('#user-input');
-const conversationElem = document.querySelector('#conversation-container');
+const messageInput = document.querySelector("#user-input");
+const conversationElem = document.querySelector("#conversation-container");
 
 // focus the input on load
 const handleFocus = () => {
@@ -9,24 +9,24 @@ const handleFocus = () => {
 // updateConversation expects an object with 'user' and 'text'
 const updateConversation = (message) => {
   const { author, text } = message;
-  const messageElem = document.createElement('p');
+  const messageElem = document.createElement("p");
 
-  messageElem.classList.add('message', author);
+  messageElem.classList.add("message", author);
   messageElem.innerHTML = `<span>${text}</span>`;
   conversationElem.appendChild(messageElem);
   conversationElem.scrollTop = conversationElem.scrollHeight;
 
-  if (author === 'user') messageInput.value = '';
+  if (author === "user") messageInput.value = "";
   handleFocus();
 };
 
 const sendMessage = (event) => {
   event.preventDefault();
+  const text = messageInput.value; // text preserves the intial value of nothing on line 19//
+  const message = { author: "user", text: text };
+  updateConversation(message); // then messageInput.value is cleared when passed.
 
-  const message = { author: 'user', text: messageInput.value };
-  updateConversation(message);
-
-  fetch('/parrot-message')
+  fetch(`/parrot-message/?color=${text}`)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
